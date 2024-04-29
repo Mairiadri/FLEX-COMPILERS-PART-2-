087b4e0ef58a2482b13ab8948a10683124e1c833
Μαρια Μπουντουρακη <marbountouraki@gmail.com>
2024-04-29 23:45:07 +0300

    1.1 1.2 1.3.2 1.3.5

--- token.h
+++ token.h
@@ -1,3 +1,12 @@
 #define DELIMITER 1
 #define INTCONST 2
+#define KEYWORD 3
+#define IDENTIFIER 4
+#define STRING 5
+#define INTEGER 6
+#define OPERATOR 7
+#define COMMENT 8
+#define MULTICOMMENT 9
+#define WHITE_SPACES 10
 /*#define FILL ME */
+

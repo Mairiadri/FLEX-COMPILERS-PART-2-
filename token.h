3dc8d8abca1d137eec6e22cee7f13a72452a0301
Μαρια Μπουντουρακη <marbountouraki@gmail.com>
2024-04-30 03:31:35 +0300

    keywords fix

--- token.h
+++ token.h
@@ -1,11 +1,10 @@
 #define WHITE_SPACES 1
-#define INTEGER 2
-#define IDENTIFIER 3
-#define KEYWORD 4
-#define FLOAT 5
-#define COMMENT 6
-#define STRING 7
-#define OPERATOR 8
-#define DELIMITER 9
-/*#define FILL ME */
+#define INTEGER      2
+#define KEYWORD      3
+#define IDENTIFIER   4
+#define FLOAT        5
+#define COMMENT      6
+#define STRING       7
+#define OPERATOR     8
+#define DELIMITER    9
 
